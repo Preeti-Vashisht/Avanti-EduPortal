6f4be0f8643f6d816de03ab22102f681acda7fec
Mohitvashist <vashisht.mohit50@gmail.com>
2021-07-21 02:24:38 +0530

    New Commit

--- src/Components/ImagePreview/index.js
+++ src/Components/ImagePreview/index.js
@@ -1,116 +1,88 @@
 import React, { useState } from "react";
 import { Container, Button, Row, Col } from "react-bootstrap";
+import Layout from "../Layout";
 
 export default function ImagePreview(props) {
-  const styles = {
-    retake: {
-      fontFamily: "IBM Plex Sans",
-      fontStyle: "normal",
-      fontWeight: "normal",
-      fontSize: "20px",
-      lineHeight: "26px",
-      alignItems: "center",
-      textAlign: "center",
-      color: "#FFFFFF",
-      float: "left",
-      background: "rgba(78, 96, 92, 1)",
-      padding: "6px",
-      borderRadius: "14px",
-      margin: "8px",
-    },
-    continue: {
-      fontFamily: "IBM Plex Sans",
-      fontStyle: "normal",
-      fontWeight: "normal",
-      fontSize: "20px",
-      lineHeight: "26px",
-      alignItems: "center",
-      align: "center",
-      color: "#000000",
-      background: "rgba(39, 164, 141, 1)",
-      padding: "6px",
-      borderRadius: "14px",
-      margin: "8px",
-    },
-    delete: {
-      fontFamily: "IBM Plex Sans",
-      fontStyle: "normal",
-      fontWeight: "normal",
-      fontSize: "20px",
-      lineHeight: "26px",
-      alignItems: "center",
-      align: "center",
-      color: "#000000",
-      margin: "8px",
-      background: "rgba(197, 61, 61, 1)",
-      borderRadius: "14px",
-      padding: "6px",
-    },
-  };
   return (
-    <Container className="text-center">
-      <Row id="viewAssigmnetHeader">
-        <Col md={{ span: 4, offset: 4 }}>
-          <h3
-            style={{
-              marginTop: "9px",
-              marginBottom: "0.1rem",
-              fontSize: "25px",
-              fontWeight: "200",
-            }}
-          >
-            निश्चित करे कि आप अपना उत्तर पढ़ सकते हैं
-          </h3>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          {props.file.url && (
-            <img
-              className="img-fluid"
-              src={props.file.url}
-              alt={props.file.name}
-            ></img>
-          )}
-        </Col>
-      </Row>
-      <Row style={{ margin: "15px", backgroundColor: "rgba(0, 0, 0, 0.21)" }}>
-        <input
-          type="file"
-          ref={props.fileInput}
-          onChange={props.assignmentRetake}
-          style={{ display: "none" }}
-        />
-        <Button
-          variant="primary"
-          size="md"
-          style={styles.retake}
-          onClick={(event) => {
-            props.fileInput.current.click();
-          }}
-        >
-          Retake
-        </Button>
+    <div>
+      <div
+        style={{
+          textAlign: "center",
+          height: "5rem",
 
-        <Button
-          variant="primary"
-          size="md"
-          className="pull-right"
-          style={styles.continue}
-          onClick={() => props.setShowAssignment(false)}
-        >
-          Continue
-        </Button>
-        <Button
-          variant="primary"
-          size="md"
-          className="pull-right"
-          style={styles.delete}
-          onClick={() => props.assignmentDelete()}
-        >
-          Delete
-        </Button>
-      </Row>
-    </Container>
+          backgroundColor: "rgba(0, 0, 0, 0.21)",
+        }}
+      >
+        <h2>निश्चित करे कि आप अपना उत्तर पढ़ सकते हैं</h2>
+      </div>
+      <div style={{ textAlign: "center", height: "90%" }}>
+        {props.file.url && (
+          <img
+            className="img-fluid"
+            src={props.file.url}
+            alt={props.file.name}
+            style={{ maxHeight: "100%", maxWidth: "100%" }}
+          ></img>
+        )}
+      </div>
+      <div
+        style={{
+          height: "5rem",
+          lineHeight: "80px",
+          textAlign: "center",
+          backgroundColor: "rgba(0, 0, 0, 0.21)",
+        }}
+      >
+        <div class="container ">
+          <div class="d-flex ">
+            <div class="align-self-start mr-auto">
+              <input
+                type="file"
+                ref={props.fileInput}
+                onChange={props.assignmentRetake}
+                style={{ display: "none" }}
+              />
+              <button
+                type="button"
+                class="btn "
+                style={{
+                  backgroundColor: "#4E605C",
+                  borderRadius: "14px",
+                  margin: "10px",
+                }}
+                onClick={(event) => {
+                  props.fileInput.current.click();
+                }}
+              >
+                Retake
+              </button>
+            </div>
+
+            <div class="align-self-end ml-auto">
+              <button
+                type="button"
+                class="btn "
+                onClick={() => props.assignmentDelete()}
+                style={{
+                  backgroundColor: "#C53D3D",
+                  borderRadius: "14px",
+                  margin: "10px",
+                }}
+              >
+                Delete
+              </button>
+              <button
+                type="button"
+                class="btn "
+                onClick={() => props.setShowAssignment(false)}
+                style={{ backgroundColor: "#27A48D", borderRadius: "14px" }}
+              >
+                Continue
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
--- src/Components/ImageSlider/index.js
+++ src/Components/ImageSlider/index.js
@@ -1,118 +1,76 @@
 import { useHistory } from "react-router-dom";
 import { Container, Button, Row, Col } from "react-bootstrap";
-export default function ViewAssignmentSlider(props) {
+export default function ImageSlider(props) {
   let history = useHistory();
-  const styles = {
-    retake: {
-      fontFamily: "IBM Plex Sans",
-      fontStyle: "normal",
-      fontWeight: "normal",
-      fontSize: "20px",
-      lineHeight: "26px",
-      alignItems: "center",
-      textAlign: "center",
-      color: "#FFFFFF",
-      float: "left",
-      background: "rgba(78, 96, 92, 1)",
-      padding: "6px",
-      borderRadius: "14px",
-      margin: "8px",
-    },
-    continue: {
-      fontFamily: "IBM Plex Sans",
-      fontStyle: "normal",
-      fontWeight: "normal",
-      fontSize: "20px",
-      lineHeight: "26px",
-      alignItems: "center",
-      align: "center",
-      color: "#000000",
-      background: "rgba(39, 164, 141, 1)",
-      padding: "6px",
-      borderRadius: "14px",
-      margin: "8px",
-    },
-    delete: {
-      fontFamily: "IBM Plex Sans",
-      fontStyle: "normal",
-      fontWeight: "normal",
-      fontSize: "20px",
-      lineHeight: "26px",
-      alignItems: "center",
-      align: "center",
-      color: "#000000",
-      margin: "8px",
-      background: "rgba(197, 61, 61, 1)",
-      borderRadius: "14px",
-      padding: "6px",
-    },
-  };
+
   return (
-    <Container className="text-center">
-      <Row id="viewAssigmnetHeader">
-        <Col md={{ span: 4, offset: 4 }}>
-          <h3
-            style={{
-              marginTop: "9px",
-              marginBottom: "0.1rem",
-              fontSize: "25px",
-              fontWeight: "200",
-            }}
-          >
-            निश्चित करे कि आप अपना उत्तर पढ़ सकते हैं
-          </h3>
-        </Col>
-      </Row>
-      <Row id="viewAssigmnetCanvas">
-        <Col>
-          <Button
-            style={styles.continue}
-            onClick={(event) => props.viewPrevAssignment()}
-          >
-            pre
-          </Button>
-          {props.file.url && (
-            <img
-              className="img-fluid"
-              src={props.file.url}
-              alt={props.file.name}
-            ></img>
-          )}
-          <Button
-            style={styles.continue}
-            onClick={(event) => props.viewNextAssignment()}
-          >
-            next
-          </Button>
-        </Col>
-      </Row>
-      <Row
-        id="viewAssigmnetFooter"
+    <div>
+      <div
         style={{
-          background: "rgba(0, 0, 0, 0.21)",
-          position: "absolute",
-          width: "100%",
+          textAlign: "center",
+          height: "5rem",
+
+          backgroundColor: "rgba(0, 0, 0, 0.21)",
         }}
       >
-        <Button
-          variant="primary"
-          size="md"
-          style={styles.retake}
-          onClick={(event) => {
-            props.setShowAssignmentSlider(false);
-          }}
-        >
-          Back
-        </Button>
-        <Button
-          variant="primary"
-          size="md"
-          style={styles.delete}
-          onClick={(event) => history.push("/assignments?submit=true")}
-        >
-          Submit Assignment
-        </Button>
-      </Row>
-    </Container>
+        <h2>निश्चित करे कि आप अपना उत्तर पढ़ सकते हैं</h2>
+      </div>
+
+      <div style={{ textAlign: "center", height: "90%" }}>
+        {props.file.url && (
+          <img
+            className="img-fluid"
+            src={props.file.url}
+            alt={props.file.name}
+            style={{ maxHeight: "100%", maxWidth: "100%" }}
+          ></img>
+        )}
+      </div>
+
+      <div
+        style={{
+          height: "5rem",
+          lineHeight: "80px",
+          textAlign: "center",
+          backgroundColor: "rgba(0, 0, 0, 0.21)",
+        }}
+      >
+        <div class="container ">
+          <div class="d-flex ">
+            <div class="align-self-start mr-auto">
+              <button
+                type="button"
+                class="btn "
+                style={{
+                  backgroundColor: "#4E605C",
+                  borderRadius: "14px",
+                  margin: "10px",
+                }}
+                onClick={(event) => {
+                  props.setShowAssignmentSlider(false);
+                }}
+              >
+                Back
+              </button>
+            </div>
+
+            <div class="align-self-end ml-auto">
+              <button
+                type="button"
+                class="btn "
+                onClick={(event) => history.push("/assignments?submit=true")}
+                style={{
+                  backgroundColor: "#C53D3D",
+                  borderRadius: "14px",
+                  margin: "10px",
+                }}
+              >
+                Submit Assignment
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
